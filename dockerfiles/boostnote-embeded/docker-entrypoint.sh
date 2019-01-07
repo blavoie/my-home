38e44f1e818c55c78e7ee79c27eb5942ad538ee8
Bruno Lavoie <bl@brunol.com>
2019-01-07 11:04:46 -0500

    Adjust some stuff

--- dockerfiles/boostnote-embeded/docker-entrypoint.sh
+++ dockerfiles/boostnote-embeded/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-USER_NAME=${USER_NAME:-developer}
-DATA_DIR=/boostnote-data
+USER_NAME=${USER_NAME:-boostnote}
 
 id -u ${USER_NAME} > /dev/null 2>&1
 if [ $? -eq 1 ]; then
+
     # Check for unset environment variables
     if [[ -z ${USER_ID} || -z ${GROUP_ID} ]]; then
         echo "USER_ID and GROUP_ID environment variables must be set."
@@ -15,11 +15,6 @@ if [ $? -eq 1 ]; then
     groupadd -g ${GROUP_ID} ${USER_NAME}
     useradd -u ${USER_ID} -g ${GROUP_ID} --create-home ${USER_NAME} > /dev/null 2>&1
     chown ${USER_ID}:${GROUP_ID} /home/${USER_NAME}
-
-    # Create/bind data directory
-    mkdir -p ${DATA_DIR}
-    chown ${USER_ID}:${GROUP_ID} ${DATA_DIR}
-    gosu ${USER_NAME} ln -s ${DATA_DIR} /home/${USER_NAME}/data
 
 fi
 
