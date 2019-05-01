451e42f1554f81d9ce7a98be637eb1c9de83e97d
Bruno Lavoie <bl@brunol.com>
2019-05-01 08:43:57 -0400

    Rename boostnote docker and fix kickstart scripts.

--- configs/kickstart/generate.sh
+++ configs/kickstart/generate.sh
@@ -6,32 +6,28 @@ while read LINE; do
     echo $LINE
 
     HOSTNAME=$(echo $LINE | cut -f1 -d ',')
-    IP=$(echo $LINE | cut -f2 -d ',')   
+    IP=$(echo $LINE | cut -f2 -d ',')
 
     NEWFILE="kickstart.$HOSTNAME.cfg"
     cp kickstart.cfg ./target/$NEWFILE
 
     sed -i "s/localhost.localdomain/$HOSTNAME/g" ./target/$NEWFILE
     sed -i "s/192.168.100.10/$IP/g"              ./target/$NEWFILE
-    
+
     virt-install \
         --connect qemu:///system \
         --name $HOSTNAME \
-        --memory memory=1024,maxmemory=4028 \
+        --memory memory=1536,maxmemory=4028 \
         --vcpus=2 \
         --disk size=8,bus=virtio,cache=writeback \
-        --network=network:openstack \
+        --network=network:default \
         --sound=none \
         --os-type=linux \
         --os-variant centos7.0 \
         --location http://centos.mirror.iweb.ca/7/os/x86_64/ \
         --initrd-inject ./target/$NEWFILE \
         --extra-args "ks=file:/$NEWFILE ip=dhcp proxy='http://gateway:3128'" &
-    
+
     sleep 25
 
 done < generate.hosts.txt
-
-
-
-
